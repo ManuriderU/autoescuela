49ee7f24ca33b62228e33a55699f27ae9abe8d83
Maria Martin Tadeo <maria.martin98@educa.madrid.org>
2024-12-16 08:21:25 +0000

    finalizacion models

--- models/models.py
+++ models/models.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
  from odoo import models, fields, api
+ from datetime import datetime, date
 
 #Definimos el modelo de datos
 class autoescuela(models.Model):
@@ -12,9 +13,9 @@ class autoescuela(models.Model):
      domicilio = fields.Char()
      localidad = fields.Char()
      provincia = fields.Char()
-     examen_ids = fields.M2M()
-     profesor_ids = fields.O2M()
-     alumno_ids = fields.O2M()
+     examen_ids = fields.Many2many()
+     profesor_ids = fields.One2many()
+     alumno_ids = fields.One2many()
      contacto = fields.Char()
      
 #     value = fields.Integer()
@@ -31,6 +32,21 @@ class profesor(models.Model):
     # Nombre y descripcion del modelo de datos
      _name = 'autoescuela.profesor'
      _description = 'autoescuela.autoescuela'
+        name = fields.Char(required=True)
+        dni= fields.Char(required=True)
+        coche = fields.Char()
+        matricula = fields.Char()
+        autoescuela_id = fields.Many2one()
+        incorporacion = fields.Date()
+        antiguedad = fields.Integer(compute = "calcular_antiguedad", store = True)
+        alumno_ids = fields.One2many()
+        
+    @api.depends('incorporacion') 
+    def calcular_antiguedad(self)
+        for record in self:
+            fecha = date.today()
+            record.antiguedad = fecha.years - record.incorporacion.years 
+        
 
 
 #Definimos el modelo de datos
@@ -38,6 +54,13 @@ class alumno(models.Model):
     # Nombre y descripcion del modelo de datos
      _name = 'autoescuela.alumno'
      _description = 'autoescuela.autoescuela'
+     name = fields.Char(required=True)
+     dni = fields.Char(required=True)
+    autoescuela_id = fields.Many2one()
+    profesor_id = fields.Many2one()
+    examenes_ids = fields.One2many()
+    domicilio = fields.Char()
+    matricula = fields.Char()
 
 
 #Definimos la clase examen
@@ -45,8 +68,19 @@ class examen(models.Model):
     # Nombre y descripcion del modelo de datos
      _name = 'examen.autoescuela'
      _description = 'autoescuela.autoescuela'
-     
+     name = fields.Char(compute = "create", store = True)
+     fecha = fields.Date()
+     autoescuela_ids = field.Many2many()
+     alumno_id = fields.Many2one(required= True)
+     moneda_id= fields.Many2one('res.currency', string="Moneda")
      precio = fields.Float()    # en el enunciado de la practica pone tipo Monetary pero da fallo
+     clases= fields.Integer()
+     carnet = fields.Char(required=True, help="AM = Ciclomotores (hasta 50 cc), \
+    A1 = Motocicletas (potencia máxima 11 KW), \
+    A2 = Motocicletas (potencia máxima 35 KW), \
+    A = Motocicletas y triciclos de motor, \
+    B = Automóviles (MMA hasta 3.500 kg)")
+     aprobado = fields.Boolean()
 
     #Pag 10 - se incluye la funcionalidad para generar la secuencia para el código de examen
     @api.model_create_multi
@@ -62,4 +96,5 @@ class autoescuela_examen(models.Model):
     # Nombre y descripcion del modelo de datos
      _name = 'autoescuela.autoescuela_examen'
      _description = 'autoescuela.autoescuela'
-
+    examen_ids = fields.Many2many()
+    autoescuela_ids = field.Many2many()
